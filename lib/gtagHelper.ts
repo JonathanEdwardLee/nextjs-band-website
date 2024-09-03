296cb47c77fac93ac7d10118fab842aff422aa4e
Jonathan Edward Lee <94152189+JonathanEdwardLee@users.noreply.github.com>
2024-09-03 11:20:25 -0500

    gtag bug fix

--- lib/gtagHelper.ts
+++ lib/gtagHelper.ts
@@ -1,3 +1,9 @@
+declare global {
+  interface Window {
+    gtag: (command: string, GA_MEASUREMENT_ID: string, config: object) => void;
+  }
+}
+
 export const pageview = (GA_MEASUREMENT_ID: string, url: string) => {
   window.gtag('config', GA_MEASUREMENT_ID, {
     page_path: url,
